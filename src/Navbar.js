10b9e6f91c11018d8e5ee57701ab4785ce31b34c
James B <jvb.codes@gmail.com>
2023-06-10 21:28:31 +0900

    Tweaked Routes

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,8 +10,8 @@ export default function Navbar() {
     <>
       <div className={styles.logo}>aita</div>
       <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/drill-maker">Drill Maker</NavLink>
+        <NavLink to="/aita">Home</NavLink>
+        <NavLink to="drill-maker">Drill Maker</NavLink>
       </nav>
       {state.isMenuOpen ? (
         ""
